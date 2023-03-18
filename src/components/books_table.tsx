0c708ca8188d4b5959a451f94db1f61566e6552f
Philippe Fanaro <philippefanaro@gmail.com>
2023-03-18 14:57:20 -0300

    better project organization

---
+++ src/components/books_table.tsx
@@ -0,0 +1,79 @@
+import Image from "next/image";
+
+import { Typography } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+import Book from "@/models/resources/book";
+
+const booksTableColumns: GridColDef[] = [
+  {
+    field: "title",
+    headerName: "Title",
+    width: 530,
+    renderCell: (params) => (
+      <>
+        <Image
+          src={params.row.pic.toString()}
+          alt="Book's Thumbnail"
+          width={50}
+          height={50}
+        />
+        <Typography sx={{ ml: 2 }} variant="subtitle1">
+          {params.value}
+        </Typography>
+        <Typography sx={{ ml: 2, color: "grey" }} variant="subtitle2">
+          {params.row.subtitle}
+        </Typography>
+      </>
+    ),
+  },
+  {
+    field: "author",
+    headerName: "Author",
+    width: 330,
+    renderCell: (params) => (
+      <>
+        {params.value.pic && (
+          <Image
+            src={params.value.pic?.toString()}
+            alt="Author's Picture"
+            width={50}
+            height={50}
+          />
+        )}
+        <Typography sx={{ ml: 2 }} variant="subtitle1">
+          {params.value.name}
+        </Typography>
+      </>
+    ),
+  },
+  {
+    field: "pages",
+    headerName: "# Pages",
+    width: 70,
+    align: "center",
+  },
+  {
+    field: "publishers",
+    headerName: "Publishers",
+    width: 200,
+    renderCell: (params) => (
+      <Typography variant="subtitle2">{params.value.join(", ")}</Typography>
+    ),
+  },
+];
+
+interface BooksTableProps {
+  books: readonly Book[];
+}
+
+export default function BooksTable({ books }: BooksTableProps) {
+  return (
+    <DataGrid
+      getRowId={(row) => row.title + row.pic}
+      rows={books}
+      columns={booksTableColumns}
+      rowHeight={50}
+    ></DataGrid>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,77 +1,12 @@
 import { useState, ChangeEvent } from "react";
 
 import Head from "next/head";
-import Image from "next/image";
 
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import { createTheme, FormControl, TextField, Typography } from "@mui/material";
+import { FormControl, TextField, Typography } from "@mui/material";
 
 import allBooks from "@/data/books";
 
-const theme = createTheme({
-  spacing: 4,
-});
-
-const booksTableColumns: GridColDef[] = [
-  {
-    field: "title",
-    headerName: "Title",
-    width: 530,
-    renderCell: (params) => (
-      <>
-        <Image
-          src={params.row.pic.toString()}
-          alt="Book's Thumbnail"
-          width={50}
-          height={50}
-        />
-        <Typography sx={{ ml: theme.spacing(2) }} variant="subtitle1">
-          {params.value}
-        </Typography>
-        <Typography
-          sx={{ ml: theme.spacing(2), color: "grey" }}
-          variant="subtitle2"
-        >
-          {params.row.subtitle}
-        </Typography>
-      </>
-    ),
-  },
-  {
-    field: "author",
-    headerName: "Author",
-    width: 330,
-    renderCell: (params) => (
-      <>
-        {params.value.pic && (
-          <Image
-            src={params.value.pic?.toString()}
-            alt="Author's Picture"
-            width={50}
-            height={50}
-          />
-        )}
-        <Typography sx={{ ml: theme.spacing(2) }} variant="subtitle1">
-          {params.value.name}
-        </Typography>
-      </>
-    ),
-  },
-  {
-    field: "pages",
-    headerName: "# Pages",
-    width: 70,
-    align: "center",
-  },
-  {
-    field: "publishers",
-    headerName: "Publishers",
-    width: 200,
-    renderCell: (params) => (
-      <Typography variant="subtitle2">{params.value.join(", ")}</Typography>
-    ),
-  },
-];
+import BooksTable from "@/components/books_table";
 
 export default function Home() {
   const [books, setBooks] = useState(allBooks);
@@ -104,7 +39,6 @@ export default function Home() {
         <title>Bibliogo Online</title>
         <meta name="description" content="Bibliogo Online" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main style={{ height: "100vh" }}>
@@ -120,12 +54,7 @@ export default function Home() {
 
         <Typography variant="h5">Books Table</Typography>
 
-        <DataGrid
-          getRowId={(row) => row.title + row.pic}
-          rows={books}
-          columns={booksTableColumns}
-          rowHeight={50}
-        ></DataGrid>
+        <BooksTable books={books} />
 
         {/* TODO: Authors Table */}
         {/* TODO: Articles Table */}
